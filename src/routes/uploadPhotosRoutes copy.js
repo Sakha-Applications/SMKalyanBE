1babc8672b963e7ddeae7e94ea1f92c08196bb1b
SatyabodhVarkur <varkur@gmail.com>
2025-06-25 10:55:35 +0530

    Photot Delete ROute

--- src/routes/uploadPhotosRoutes copy.js
+++ src/routes/uploadPhotosRoutes copy.js
@@ -1,17 +1,14 @@
-// D:\1. Data\1. Personal DOcument\00.SM\NewProject\dev\SMKalyanBE\src\routes\uploadPhotosRoutes.js
-
+//**2.3 Routes (`uploadPhotosRoutes.js`)**
 const express = require("express");
 const router = express.Router();
-// --- MODIFY THIS LINE ---
-const { uploadPhotos, getPhotos, getDefaultPhoto, deletePhoto } = require("../controllers/uploadPhotosController"); // Import deletePhoto
-// -----------------------
+const { uploadPhotos, getPhotos, getDefaultPhoto } = require("../controllers/uploadPhotosController");
 const fileUpload = require("express-fileupload");
 
 router.use(fileUpload({
     useTempFiles: true,
     tempFileDir: "/tmp/",
     createParentPath: true,
-}, (err) => {
+}, (err) => { // Debug: Log any error during file upload middleware setup
     if (err) {
         console.error("Debug (Routes): Error setting up file upload middleware:", err);
     } else {
@@ -19,11 +16,12 @@ router.use(fileUpload({
     }
 }));
 
+// Add debug logs here
 router.post("/upload-photos", (req, res, next) => {
     console.log("Debug (Routes): Incoming request to /upload-photos");
     console.log("Debug (Routes): Request body:", req.body);
     console.log("Debug (Routes): Request files:", req.files);
-    next();
+    next(); // Pass control to the next handler (uploadPhotos)
 }, uploadPhotos);
 
 router.get("/get-photos", (req, res, next) => {
@@ -37,13 +35,5 @@ router.get("/get-default-photo", (req, res, next) => {
     console.log("Debug (Routes): Query parameters:", req.query);
     next();
 }, getDefaultPhoto);
-
-// --- ADD THIS NEW ROUTE FOR PHOTO DELETION ---
-router.delete("/delete-photo", (req, res, next) => {
-    console.log("Debug (Routes): Incoming request to /delete-photo");
-    console.log("Debug (Routes): Query parameters:", req.query); // Log incoming query params
-    next();
-}, deletePhoto);
-// ---------------------------------------------
 
 module.exports = router;
